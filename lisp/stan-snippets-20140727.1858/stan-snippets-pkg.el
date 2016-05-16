205e0030016d8d978ab0ae0b8087d6b1706d76a8
Roy Emanuel <roy.emanuel@jhuapl.edu>
2016-05-16 13:00:11 -0400

    Current Configuration

---
+++ lisp/stan-snippets-20140727.1858/stan-snippets-pkg.el
@@ -0,0 +1,8 @@
+(define-package "stan-snippets" "20140727.1858" "Yasnippets for Stan"
+  '((yasnippet "0.8.0")
+    (stan-mode "2.4.1"))
+  :url "http://github.com/stan-dev/stan-mode" :keywords
+  '("languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
